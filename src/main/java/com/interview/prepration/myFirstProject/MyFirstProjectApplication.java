31697093d755c6678889be4aee4a17877154c216
DeveloperSummit <116750933+DeveloperSummit@users.noreply.github.com>
2025-04-15 01:19:14 +0530

    Update MyFirstProjectApplication.java
    TestCommit

--- src/main/java/com/interview/prepration/myFirstProject/MyFirstProjectApplication.java
+++ src/main/java/com/interview/prepration/myFirstProject/MyFirstProjectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyFirstProjectApplication {
-
+	System.out.println("hello");
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstProjectApplication.class, args);
 	}
